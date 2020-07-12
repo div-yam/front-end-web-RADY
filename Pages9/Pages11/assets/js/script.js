91330ae81520103d9f1fef770581e6ffd1007c28
div-yam <divyamagarwal24@gmail.com>
2020-07-12 20:46:32 +0530

    Front End Code Uploaded

---
+++ Pages9/Pages11/assets/js/script.js
@@ -0,0 +1,16 @@
+function myFunction1() {
+  document.getElementById("idproofrow").style.display = 'none';
+    document.getElementById("shiftrow").style.display = 'none';
+    document.getElementById("specialrow").style.display = 'none';
+     document.getElementById("form").style.display = 'block';
+     document.getElementById("submitbutton").style.display = 'block';
+    
+}
+function myFunction2() {
+  document.getElementById("idproofrow").style.display = 'none';
+    document.getElementById("shiftrow").style.display = 'none';
+    document.getElementById("specialrow").style.display = 'none';
+    document.getElementById("form").style.display = 'block';
+     document.getElementById("submitbutton").style.display = 'block';
+    
+} 
